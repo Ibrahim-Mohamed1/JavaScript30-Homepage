5bec2a8aafa5aacf33063a04ba8ceef980238cac
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-05-25 20:00:04 -0600

    app 11 added

--- src/Home.js
+++ src/Home.js
@@ -232,8 +232,8 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="project11" style={{ cursor: "pointer", backgroundSize: "cover", backgroundRepeat: "no-repeat", height: "vh" }}>
-                    <div className='middle'>
-                        <a href="/">
+                    <div className='middle1'>
+                        <a href="http://app11-javascript30.surge.sh/">
                             <div className='projectInfo' style={{ marginTop: "-1.5em" }}>
                                 <h2 className="nav" style={{ marginTop: 0, color: "cyan", padding: "2%" }}>
                                     11. Custom Video Player
