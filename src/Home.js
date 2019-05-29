1d653a753122394830ed87bac30525e91206037c
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-05-28 21:35:43 -0600

    updatee

--- src/Home.js
+++ src/Home.js
@@ -292,7 +292,7 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="project14" style={{ cursor: "pointer", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
-                    <div className='middle'>
+                    <div className='middle1'>
                         <a href="/">
                             <div className='projectInfo' style={{ marginTop: "-1.5em" }}>
                                 <h2 className="nav" style={{ marginTop: 0, color: "cyan", padding: "2%" }}>14. Coming Soon...</h2>
