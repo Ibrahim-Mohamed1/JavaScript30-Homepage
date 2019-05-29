931aae944e5e293990046e00c3bae311303626a6
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-05-28 21:34:54 -0600

    updatee

--- src/Home.js
+++ src/Home.js
@@ -272,7 +272,7 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="project13" style={{ cursor: "pointer", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
-                    <div className='middle1'>
+                    <div className='middle'>
                         <a href="http://app13-javascript30.surge.sh/">
                             <div className='projectInfo' style={{ marginTop: "-1.5em" }}>
                                 <h2 className="nav" style={{ marginTop: 0, color: "cyan", padding: "2%" }}>
