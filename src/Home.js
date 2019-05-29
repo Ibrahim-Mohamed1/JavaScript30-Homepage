a4d35cc8bb2de60a163069318a2c2967c016dde7
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-05-28 21:35:20 -0600

    updatee

--- src/Home.js
+++ src/Home.js
@@ -273,7 +273,7 @@ const Home = () => {
                 </div>
                 <div className="project13" style={{ cursor: "pointer", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
                     <div className='middle1'>
-                        <a href="/">
+                        <a href="http://app13-javascript30.surge.sh/">
                             <div className='projectInfo' style={{ marginTop: "-1.5em" }}>
                                 <h2 className="nav" style={{ marginTop: 0, color: "cyan", padding: "2%" }}>
                                     13. Slide In On Scroll
