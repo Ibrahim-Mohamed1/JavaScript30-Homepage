c6823aa54d287359e574b4508054b4578023a367
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-07-05 22:43:15 -0600

    update

--- src/Home.js
+++ src/Home.js
@@ -475,12 +475,18 @@ const Home = () => {
                     <div className='middle1'>
                         <a href="http://app23-javascript30.netlify.com/">
                             <div className='projectInfo' style={{ marginTop: "-1.5em" }}>
-                                <h2 className="nav" style={{ marginTop: 0, color: "cyan", padding: "2%" }}>23. Coming Soon...</h2>
-                                {/* <h4 style={{ padding: 5 }}>
+                                <h2 className="nav" style={{ marginTop: 0, color: "cyan", padding: "2%" }}>
+                                    23. Coming Soon...
+                                </h2>
+                                <h4 style={{ padding: 5 }}>
                                     Simple app to amuse users with quotes by Kanye West.
                                 </h4>
-                                <p style={{ color: "lime", padding: 5 }}>JavaScript, React, JSX, Axios, Kanye API, HTML, & CSS.</p>
-                                <p style={{ margin: 0, marginRight: 5 }}>Made: 05/6/2019</p> */}
+                                <p style={{ color: "lime", padding: 5 }}>
+                                    JavaScript, HTML, & CSS.
+                                </p>
+                                <p style={{ margin: 0, marginRight: 5 }}>
+                                    Made: 06/6/2019
+                                </p>
                             </div>
                         </a>
                     </div>
