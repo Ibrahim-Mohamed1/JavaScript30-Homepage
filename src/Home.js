2ca9f05952a1d7ba1df4dd743d0f99ade7242f4f
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-07-05 22:48:48 -0600

    update

--- src/Home.js
+++ src/Home.js
@@ -575,12 +575,18 @@ const Home = () => {
                     <div className='middle1'>
                         <a href="http://app28-javascript30.netlify.com/">
                             <div className='projectInfo' style={{ marginTop: "-1.5em" }}>
-                                <h2 className="nav" style={{ marginTop: 0, color: "cyan", padding: "2%" }}>28. Coming Soon...</h2>
-                                {/* <h4 style={{ padding: 5 }}>
+                                <h2 className="nav" style={{ marginTop: 0, color: "cyan", padding: "2%" }}>
+                                    28. Coming Soon...
+                                </h2>
+                                <h4 style={{ padding: 5 }}>
                                     Simple app to help users find search and find desired content on a replica of Google.
                                 </h4>
-                                <p style={{ color: "lime", padding: 5 }}>JavaScript, React, JSX, Axios, FAROO Web Search API, HTML, & CSS.</p>
-                                <p style={{ margin: 0, marginRight: 5 }}>Made: 05/11/2019</p> */}
+                                <p style={{ color: "lime", padding: 5 }}>
+                                    JavaScript, HTML, & CSS.
+                                </p>
+                                <p style={{ margin: 0, marginRight: 5 }}>
+                                    Made: 06/11/2019
+                                </p>
                             </div>
                         </a>
                     </div>
