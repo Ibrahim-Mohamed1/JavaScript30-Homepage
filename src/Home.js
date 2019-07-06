89b74dfc9685605b44a6ea9d18ef7e6ebbb2422b
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-07-05 18:14:34 -0600

    update

--- src/Home.js
+++ src/Home.js
@@ -413,7 +413,7 @@ const Home = () => {
                 </div>
                 <div className="project20" style={{ cursor: "pointer", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}>
                     <div className='middle1'>
-                        <a href="/">
+                        <a href="http://app20-javascript30.netlify.com/">
                             <div className='projectInfo' style={{ marginTop: "-1.5em" }}>
                                 <h2 className="nav" style={{ marginTop: 0, color: "cyan", padding: "2%" }}>20. Coming Soon...</h2>
                                 <h4 style={{ padding: 5 }}>
