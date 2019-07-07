f899bf7cf038246a6e4a200718acfcac83eaa571
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-07-06 23:36:03 -0600

    update

--- src/Home.js
+++ src/Home.js
@@ -505,7 +505,7 @@ const Home = () => {
                                     JavaScript, HTML, & CSS.
                                 </p>
                                 <p style={{ margin: 0, marginRight: 5 }}>
-                                    Made: 06/7/2019
+                                    Made: 06/07/2019
                                 </p>
                             </div>
                         </a>
